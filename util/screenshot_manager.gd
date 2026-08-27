5f969781773becca90c41ed710da5cf660200334
George <winkassador+github@gmail.com>
2026-08-27 16:27:27 +0100

    Allow screenshots to be taken on web

--- util/screenshot_manager.gd
+++ util/screenshot_manager.gd
@@ -6,11 +6,24 @@ signal screenshot_taken(location: String)
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.is_action_pressed("screenshot"):
-			var dir = OS.get_system_dir(OS.SYSTEM_DIR_PICTURES) + "/Gravity Engine/"
-			if not DirAccess.dir_exists_absolute(dir):
-				DirAccess.make_dir_absolute(dir)
 			var filename = Time.get_datetime_string_from_system() + ".png"
-			var path = dir + filename
-			get_viewport().get_texture().get_image().save_png(path)
-			print("Screenshot saved as \"%s\"" % path)
-			screenshot_taken.emit(path)
+			var image = get_viewport().get_texture().get_image()
+			if OS.get_name() == "Web":
+				_save_screenshot_from_web(image, filename)
+			else:
+				_save_screenshot_from_desktop(image, filename)
+
+func _save_screenshot_from_web(image: Image, filename: String) -> void:
+	var buffer := image.save_png_to_buffer()
+	JavaScriptBridge.download_buffer(buffer, filename, "image/png")
+	print("Screenshot saved as \"%s\"" % filename)
+	screenshot_taken.emit(filename)
+
+func _save_screenshot_from_desktop(image: Image, filename: String) -> void:
+	var dir = OS.get_system_dir(OS.SYSTEM_DIR_PICTURES) + "/Gravity Engine/"
+	if not DirAccess.dir_exists_absolute(dir):
+		DirAccess.make_dir_absolute(dir)
+	var path = dir + filename
+	image.save_png(path)
+	print("Screenshot saved as \"%s\"" % path)
+	screenshot_taken.emit(path)
