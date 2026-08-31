cf148f0765468d46112365967fff416956d751b3
George <winkassador+github@gmail.com>
2026-08-31 22:41:09 +0100

    Minor performance improvements to FastCPU

--- rust/src/simulation.rs
+++ rust/src/simulation.rs
@@ -16,7 +16,7 @@ struct FastCPUSimulation {
 #[godot_api]
 impl FastCPUSimulation {
     #[func]
-    fn setup(&self, config: Gd<Resource>) {}
+    fn setup(&self, _config: Gd<Resource>) {}
 
     #[func]
     fn step(&mut self, steps: i32, config: Gd<Resource>) {
@@ -27,20 +27,20 @@ impl FastCPUSimulation {
         let gravity: f32 = config.get("gravity").to::<f32>();
         
         for _step in 0..steps {
-            self._half_kick(timestep);
-            self._drift(timestep);
-            self._accelerate(softening, gravity);
-            self._half_kick(timestep);
+            self.half_kick(timestep);
+            self.drift(timestep);
+            self.accelerate(softening, gravity);
+            self.half_kick(timestep);
         }
     }
 
-    fn _half_kick(&mut self, timestep: f32) {
+    fn half_kick(&mut self, timestep: f32) {
         for i in 0..self.positions.len() {
             self.velocities[i] += self.accelerations[i] * timestep * 0.5;
         }
     }
 
-    fn _drift(&mut self, timestep: f32) {
+    fn drift(&mut self, timestep: f32) {
         for i in 0..self.positions.len() {
             self.positions[i] += self.velocities[i] * timestep;
             //_constrain(i, config)
@@ -48,19 +48,19 @@ impl FastCPUSimulation {
         }
     }
 
-    fn _accelerate(&mut self, softening: f32, gravity: f32) {
+    fn accelerate(&mut self, softening: f32, gravity: f32) {
         let particle_count: i32 = self.positions.len() as i32;
+        let softening_sq = softening * softening;
 
         for i in 0..particle_count {
             let p1 = self.positions[i as usize];
             let m1 = self.masses[i as usize];
             for j in (i + 1)..particle_count {
-                let p2 = self.positions[j as usize];
                 let m2 = self.masses[j as usize];
-                let d = p2 - p1;
-                
+
+                let d = self.positions[j as usize] - p1;
                 let dist_sq = d.length_squared();
-                let r = (dist_sq + softening * softening).sqrt();
+                let r = (dist_sq + softening_sq).sqrt();
                 
                 let acc = gravity / (r * r * r) * d;
                 
